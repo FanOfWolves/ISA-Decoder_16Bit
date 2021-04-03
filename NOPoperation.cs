f0934614b8d99cdc589e4ff194c4acc29769d67b
ed4grrr <edgarbowlinthethird@gmail.com>
2021-04-02 23:24:47 -0400

    vghfkj

--- NOPoperation.cs
+++ NOPoperation.cs
@@ -1,4 +1,17 @@
+// ------------------------------------------------------------------------------------------------------------------------
+// File name:       NOPoperation.cs
+// Project name:    ISA
+// Project description: Decoder for our awesome Detached-Toe 16-bit RISC ISA.
+// ------------------------------------------------------------------------------------------------------------------------
+// Creator's name and email: Harrison Lee Pollitte. pollitteh@etsu.edu. Edgar Bowlin III, bowline@etsu.edu. nelsondk@etsu.edu 
+// Course Name: CSCI-4727 Computer Architecture
+// Course Section: 940
+// Creation Date: 03/31/2021
+// ------------------------------------------------------------------------------------------------------------------------
 namespace ISA_Decoder_16Bit {
+    /// <summary>
+    /// what can you really expect? this just lets the CPU spin.
+    /// </summary>
     class NOPoperation: Operation {
         string verb = "NOOP";                    // The main verb used for the message
 
@@ -18,6 +31,10 @@ namespace ISA_Decoder_16Bit {
 
         int immediateOperandStartBit = 0;       // The start bit for our immediate operand (the 2nd operand)
 
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
         public NOPoperation() {
 
         }
@@ -57,6 +74,11 @@ namespace ISA_Decoder_16Bit {
             }
         }
 
+
+        /// <summary>
+        /// decode the second operand and get its value and meaning
+        /// </summary>
+        /// <param name="inputBits">instruction bits</param>
         private void DecodeSecondOperand(int inputBits) {
             // Immediate or Register?
             if(immediateSwitchValue == (int)ImmediateSwitchEnum.immediate) { // This is an immediate value.
--- Operation.cs
+++ Operation.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿// ------------------------------------------------------------------------------------------------------------------------
+// File name:       Operation.cs
+// Project name:    ISA
+// Project description: Decoder for our awesome Detached-Toe 16-bit RISC ISA.
+// ------------------------------------------------------------------------------------------------------------------------
+// Creator's name and email: Harrison Lee Pollitte. pollitteh@etsu.edu. Edgar Bowlin III, bowline@etsu.edu. nelsondk@etsu.edu 
+// Course Name: CSCI-4727 Computer Architecture
+// Course Section: 940
+// Creation Date: 03/31/2021
+// ------------------------------------------------------------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ISA_Decoder_16Bit {
+    /// <summary>
+    /// used to give every operation the decode operation function.
+    /// </summary>
     abstract class Operation {
 
         /// <summary>
